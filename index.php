543808ded152b95fa5c9cbf15310f77bf909dcab
Katherine Watkins <watkins.katherine@student.greenriver.edu>
2023-02-02 16:55:14 -0500

    fixed merge issues

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
     <li>have a tail</li>
 </ul>
 <p>Some zoos have zebras.</p>
-<<<<<<< HEAD
-<p>This is Kat's paragraph about zebras. She thinks they are striped horses.</p>
-=======
 <p>not all zebras are in zoos!</p>
->>>>>>> f20db96976b05051c2d2b8ea755a1f8d97361689
+<p>This is Kat's paragraph about zebras. She thinks they are striped horses.</p>
+
+
+
 <?php
 ?>
