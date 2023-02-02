eeb5f124913d17725082b7b7713ff7ffba62d330
Paul Woods <paul.n.woods@gmail.com>
2023-02-02 16:17:48 -0500

    added unordered list

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 <p>This is a zebra paragraph!</p>
 <h2>Zebras have stripes.</h2>
-
+<ul>
+    <li>zebras are black and white</li>
+    <li>have hooves</li>
+    <li>have a tail</li>
+</ul>
 <?php
 ?>
